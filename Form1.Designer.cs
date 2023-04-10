d75853ab51487ac0d45009289c94a0110f76a633
DanielMonteiro22 <danielmontcosta55@gmail.com>
2023-04-10 20:59:09 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Converso
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCelius = new System.Windows.Forms.TextBox();
+            this.textBoxFahrenheit = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grau Celsius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Graus Fahrenheit";
+            // 
+            // textBoxCelius
+            // 
+            this.textBoxCelius.Location = new System.Drawing.Point(119, 88);
+            this.textBoxCelius.Name = "textBoxCelius";
+            this.textBoxCelius.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCelius.TabIndex = 2;
+            // 
+            // textBoxFahrenheit
+            // 
+            this.textBoxFahrenheit.Location = new System.Drawing.Point(340, 88);
+            this.textBoxFahrenheit.Name = "textBoxFahrenheit";
+            this.textBoxFahrenheit.Size = new System.Drawing.Size(124, 23);
+            this.textBoxFahrenheit.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Calcular);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxFahrenheit);
+            this.Controls.Add(this.textBoxCelius);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxCelius;
+        private TextBox textBoxFahrenheit;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿namespace Converso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular(object sender, EventArgs e)
+        {
+            int celsius = Convert.ToInt32(textBoxCelius.Text);
+
+            int fahrenheit = Convert.ToInt32(textBoxFahrenheit.Text);
+
+            int resultado =(32 °F − 32) × 5/9 = 0 °C;
+
+            MessageBox.Show($"A idade é {resultado} anos!! ");
+        }
+    }
+}
